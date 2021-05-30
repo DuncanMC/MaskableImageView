4ab9da42d421b1004b22a8a297452fb9a919dff6
Duncan Champney <DChampney@wareto.com>
2021-05-30 10:49:30 -0400

    Initial Commit

---
+++ MaskableImageView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MaskableImageView
+//
+//  Created by Duncan Champney on 5/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
